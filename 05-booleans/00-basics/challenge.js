aa8d8fc1a287234f59059b6b521d4299cabba0fd
Jennifer Nelson <jenniferwnelson@sbcglobal.net>
2014-10-15 03:58:58 +0000

    completed equalStrings, notEqual, inBetween, outsideRanges and nameAndPrice functions.

--- 05-booleans/00-basics/challenge.js
+++ 05-booleans/00-basics/challenge.js
@@ -1,19 +1,25 @@
 module.exports.equalStrings = function(stringOne, stringTwo) {
-
+  return stringOne == stringTwo;
 };
 
 module.exports.notEqual = function(one, two) {
-
+  return one !== two;
 };
 
 module.exports.inBetween = function(lower, middle, upper) {
-
+  return (lower < middle) && (middle < upper);
 };
 
 module.exports.outsideRanges = function(number) {
-
+  var res = (number < 10 || number > 20) &&
+            (number <= 42 || number > 75) &&
+            (number <= 1 || number >= 6);
+            
+  return res;
 };
 
 module.exports.nameAndPrice = function(name, price) {
+  var res = (name == "NYTimes" || name == "LATimes") && (price >= 1);
 
+  return res;
 };
